cdc2e3c56bc3287fc68b8905b76ca8a73d40648b
prabhasreddy1234 <prabhasreddykarri@gmail.com>
2026-02-24 11:42:26 +0530

    done

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { copyFileSync } from 'fs'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'copy-cname',
+      closeBundle() {
+        copyFileSync('CNAME', 'dist/CNAME')
+      }
+    }
+  ],
   base: '/',
 })
